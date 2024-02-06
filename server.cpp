5ecc6be16a0590faebc48b0d34ef0de2ab4a8d90
mishamashina <misha_sidorenko228@mail.ru>
2024-02-06 06:01:51 -0700

    first commit

---
+++ server.cpp
@@ -0,0 +1,81 @@
+#include "server.h"
+
+Server::Server()
+{
+    if(this->listen(QHostAddress::Any, 2323)) //Tells the server to listen for incoming connections
+    {
+        qDebug() << "Start";
+    }
+    else
+    {
+        qDebug() << "Error";
+    }
+    nextBlockSize = 0;
+}
+
+void Server::incomingConnection(qintptr socketDescriptor) //This virtual function is called by QTcpServer when a new connection is available
+{
+    socket = new QTcpSocket;
+    socket->setSocketDescriptor(socketDescriptor); //  The socketDescriptor argument is the native socket descriptor for the accepted connection
+    //qDebug() << socket->socketDescriptor();
+    connect(socket, &QTcpSocket::readyRead, this, &Server::slotReadyRead);
+    connect(socket, &QTcpSocket::disconnected, socket, &QTcpSocket::deleteLater);
+
+    Sockets.push_back(socket);
+    qDebug() << "Client connected" << socketDescriptor;
+}
+
+void Server::slotReadyRead()
+{
+    socket = (QTcpSocket*)sender();
+    QDataStream in(socket);
+    in.setVersion(QDataStream::Qt_5_15);
+    if(in.status() == QDataStream::Ok)
+    {
+        qDebug() << "read...";
+        for(;;)
+        {
+            if(nextBlockSize == 0)
+            {
+                if(socket->bytesAvailable() < 2)
+                {
+                    qDebug() << "Data < 2, break";
+                    break;
+                }
+                in >> nextBlockSize;
+                qDebug() << "nextBlockSize" << nextBlockSize;
+            }
+            if(socket->bytesAvailable() < nextBlockSize)
+            {
+                qDebug() << "Data not full, break";
+                break;
+            }
+            QString str;
+            QTime time;
+            in >> time >> str;
+            nextBlockSize = 0;
+            qDebug() << str;
+            SendToClient(str);
+            break;
+        }
+    }
+    else
+    {
+        qDebug() << "DataStrem error";
+    }
+}
+
+void Server::SendToClient(QString str)
+{
+    Data.clear();
+    QDataStream out(&Data, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_5_15);
+    out << quint16(0) << QTime::currentTime() << str;
+    out.device()->seek(0);
+    out << quint16(Data.size() - sizeof(quint16));
+    //socket->write(Data);
+    for(int i = 0; i < Sockets.size(); i++)
+    {
+        Sockets[i]->write(Data);
+    }
+}
---
+++ server.h
@@ -0,0 +1,28 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QDebug>
+#include <QDataStream>
+#include <QTime>
+
+class Server : public QTcpServer
+{
+    Q_OBJECT
+public:
+    Server();
+    QTcpSocket *socket;
+
+private:
+    QVector <QTcpSocket*> Sockets;
+    QByteArray Data;
+    quint16 nextBlockSize;
+    void SendToClient(QString str);
+
+public slots:
+    void incomingConnection(qintptr socketDescriptor);
+    void slotReadyRead();
+};
+
+#endif // SERVER_H
